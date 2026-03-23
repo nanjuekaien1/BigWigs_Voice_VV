44f2613678ce279939595eb0f1aff07115b3eee2
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2026-03-23 14:58:44 +0800

    Add files via upload

--- Core.lua
+++ Core.lua
@@ -14,10 +14,11 @@ addon.SendMessage = BigWigsLoader.SendMessage
 --
 
 local path = "Interface\\AddOns\\BigWigs_Voice_VV\\Sounds\\%s.ogg"
-local pathYou = "Interface\\AddOns\\BigWigs_Voice_VV\\Sounds\\%sy.ogg"
+--local pathYou = "Interface\\AddOns\\BigWigs_Voice_VV\\Sounds\\%sy.ogg"
 local function handler(event, module, key, sound, isOnMe)
 	--local success = PlaySoundFile(format(isOnMe and pathYou or path, tostring(key)), "Master")
-	local success = PlaySoundFile(format(isOnMe and pathYou or path, (addon.FilePaths[key] or "")..tostring(key)), "Master")
+	--local success = PlaySoundFile(format(isOnMe and pathYou or path, (addon.FilePaths[key] or "")..tostring(key)), "Master")
+	local success = PlaySoundFile(format(path, (addon.FilePaths[key] or "")..tostring(key)), "Master")
 	if not success then
 		addon:SendMessage("BigWigs_Sound", module, key, sound) 
 	end
@@ -30,6 +31,7 @@ BigWigsAPI.RegisterVoicePack("VV")
 
 local VoiceID = "中文语音：VV(女)"
 
+--addonTbl.API:RegisterCountdown(VoiceID, {
 BigWigsAPI:RegisterCountdown(VoiceID, {
    "Interface\\AddOns\\BigWigs_Voice_VV\\Media\\Sounds\\1.ogg",
    "Interface\\AddOns\\BigWigs_Voice_VV\\Media\\Sounds\\2.ogg",
@@ -42,3 +44,6 @@ BigWigsAPI:RegisterCountdown(VoiceID, {
    "Interface\\AddOns\\BigWigs_Voice_VV\\Media\\Sounds\\9.ogg",
    "Interface\\AddOns\\BigWigs_Voice_VV\\Media\\Sounds\\10.ogg",
 })
+
+
+
